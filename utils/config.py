dba49c6c799f1875458e0ae5b5f9f15ea04dcdd3
Alberto Becerra <alberto.becerra@thebridgeschool.es>
2023-03-08 11:31:21 +0100

    libreria utils: fichero configuracion

---
+++ utils/config.py
@@ -0,0 +1,23 @@
+N_LETTERS = 5
+EMPTY_SOLUTION = "_" * N_LETTERS
+
+HEAD =\
+'''
+ -------
+|       |
+|       |
+ ------- 
+'''
+
+VERTICAL = \
+'''
+    |
+'''
+
+RIGHT_EXT =\
+r'''
+     \
+      \
+'''
+
+HANGMAN = [HEAD, VERTICAL, RIGHT_EXT, VERTICAL, RIGHT_EXT]
